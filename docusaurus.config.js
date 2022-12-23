fca71dd6806639872179fcf5e99fd7f271cb2d74
acharlop <avicharlop@gmail.com>
2022-12-23 11:48:51 +0100

    first blog post

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Tez Docs',
-  tagline: 'Tezos Developer documentation done right',
+  tagline: 'Tezos dev docs done right.',
   url: 'https://tezdocs.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
